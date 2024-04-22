973d515ccb333a83b0b93cca18df796f6399f943
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-04-22 12:50:30 +0700

    change style

---
+++ SWP391_Project/SWP391_Project/Databases/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SWP391_Project.Databases.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Databases
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Role>? Roles { get; set; }
+    }
+    
+
+}
---
+++ SWP391_Project/SWP391_Project/Databases/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SWP391_Project.Databases.Models
+{
+    public class Role
+    {
+        [Column(TypeName = "char(2)")]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string RoleName { get; set; }
+    }
+}
