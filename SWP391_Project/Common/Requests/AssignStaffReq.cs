ea87a8cef666ed64e68d74662aeb7ecb11311f56
1903giabao <baogavip00@gmail.com>
2024-06-03 08:07:55 +0700

    Bao/AssignOrderDetail

---
+++ SWP391_Project/Common/Requests/AssignStaffReq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Requests
+{
+    public class AssignStaffReq
+    {
+        public int OrderDetailID { get; set; }
+        public int ValuationStaffID { get; set; }
+    }
+}
