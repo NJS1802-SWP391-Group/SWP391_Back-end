1495dd91c16af22beff8fe4c2f1fd7146d3375b4
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-06-18 12:49:41 +0700

    Add Link

--- SWP391_Project/Common/Enums/DiamondShapeEnum.cs
+++ SWP391_Project/Common/Enums/DiamondShapeEnum.cs
@@ -8,15 +8,15 @@ namespace Common.Enums
 {
     public static class DiamondShapeEnum
     {
-        public const string Round = "1";
-        public const string Oval = "2";
+        public const string Round = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fc6a7cc5d-fcfe-474f-8693-355be15070c3.jpg?alt=media&token=10dde796-ab91-4fc1-8af3-ae21d50164f4";
+        public const string Oval = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2F67d33e4d-10af-41b7-948b-72abb97c44ad.jpg?alt=media&token=19a92099-05f3-4b1b-b747-1791e22864f9";
         public const string Princess = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2F6269c793-25e4-4e57-8fea-1b0641b4bc8f.jpg?alt=media&token=cf39ed23-f2c3-4032-900c-cf14411f819a";
-        public const string Emerald = "4";
-        public const string Cushion = "5";
-        public const string Radiant = "6";
-        public const string Pear = "7";
-        public const string Heart = "8";
-        public const string Marquise = "9";
-        public const string Asscher = "10";
+        public const string Emerald = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fdfa3f593-18e1-4527-b551-ac1b4ef3a8ff.jpg?alt=media&token=c5a77e3c-26fe-44d4-81cf-378b4ebf38b9";
+        public const string Cushion = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fd8bf923e-f2ee-46a2-a824-a33c437e4b5f.jpg?alt=media&token=3cf47580-5368-49be-b4e0-5e1d2260efe0";
+        public const string Radiant = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fd631b10d-5a9c-42e4-8c2e-ed7285db6872.jpg?alt=media&token=dae78795-9ffe-408d-833c-00057650c0fe";
+        public const string Pear = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fceb6c063-3cf6-4402-b24d-202bf13aa32f.jpg?alt=media&token=7bc6eb03-08aa-4cfe-bdae-90452906e7ef";
+        public const string Heart = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2F522ede31-8a6f-4293-9fca-e5043b1cb410.jpg?alt=media&token=6fc31e05-5399-40cd-b50e-a1f52706d1b1";
+        public const string Marquise = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fd34ba206-d801-4eda-bce4-4b7a80ca41fb.jpg?alt=media&token=394193e8-5344-44a9-a875-5fc43835694d";
+        public const string Asscher = "https://firebasestorage.googleapis.com/v0/b/testproject-bc2e2.appspot.com/o/DIAMOND%2Fa3ff533d-2b4f-4ab0-b5e4-67e082487b5f.jpg?alt=media&token=a8ae4aae-d7fd-452b-8532-9eac9c9ef4bc";
     }
 }
