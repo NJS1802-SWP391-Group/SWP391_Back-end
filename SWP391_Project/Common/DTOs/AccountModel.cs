552471e1fa98be62118145ddd45b6e600c280319
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-07-22 14:32:36 +0700

    fix

--- SWP391_Project/Common/DTOs/AccountModel.cs
+++ SWP391_Project/Common/DTOs/AccountModel.cs
@@ -11,6 +11,13 @@ namespace SWP391_Project.Dtos
         public string Password { get; set; }
         public string Status { get; set; }
         public string RoleName { get; set; }
+        public string Email {  get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Cccd {  get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Dob {  get; set; }
+        public string Address { get; set; }
         public int CustomerId { get; set; }
     }
 }
